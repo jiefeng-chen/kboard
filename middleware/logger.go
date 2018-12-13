8baf6d7e59c2702a4aa0bc0c5d9e0782754200f9
chenjiefeng <chenjiefeng@4399inc.com>
2018-12-13 17:38:52 +0800

    fix bug

--- middleware/logger.go
+++ middleware/logger.go
@@ -11,7 +11,7 @@ import (
 func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if matched, _ := regexp.Match("^/assets/.*", []byte(r.RequestURI)); !matched {
-			log.Println(fmt.Sprintf("%s %s %s", r.Method, r.RequestURI, r.Proto))
+			log.Println(fmt.Sprintf("%s %s %s [token:%s]", r.Method, r.RequestURI, r.Proto, r.Header.Get("X-Session-Token")))
 		}
 		next.ServeHTTP(w, r)
 	})
