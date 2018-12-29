5969cd8ef5bb3603f78ec40261d83606b18bed51
chenjiefeng <chenjiefeng@4399inc.com>
2018-12-29 19:38:37 +0800

    update

---
+++ test/redis_test.go
@@ -0,0 +1,50 @@
+package test
+
+import (
+	"testing"
+	"github.com/chasex/redis-go-cluster"
+	"time"
+)
+
+func TestRedisNode(t *testing.T) {
+
+}
+
+func TestRedisConn(t *testing.T) {
+	clusterConn, err := redis.NewCluster(&redis.Options{
+		StartNodes: []string{
+			"192.168.37.150:8001",
+			"192.168.37.150:8002",
+			"192.168.37.150:8003",
+			"192.168.37.150:8004",
+			"192.168.37.150:8005",
+			"192.168.37.150:8006"},
+		ConnTimeout: 50 * time.Millisecond,
+		ReadTimeout: 50 * time.Millisecond,
+		WriteTimeout: 50 * time.Millisecond,
+		KeepAlive: 16,
+		AliveTime: 50 * time.Second,
+	})
+
+	if err != nil {
+		t.Fatalf("redis.New error: %s", err.Error())
+	}
+
+	_, err = clusterConn.Do("SET", "myname", "realjf")
+	if err != nil {
+		t.Errorf("%v", err)
+		return
+	}
+
+	data, err := clusterConn.Do("GET", "myname")
+	if err != nil {
+		t.Errorf("%v", err)
+		return
+	}
+
+	defer clusterConn.Close()
+
+	t.Errorf("%v", data)
+
+}
+
