b58fdb15b6ae90c28738f3c08da3a5d37dbb2dcf
chenjiefeng <chenjiefeng@4399inc.com>
2019-01-25 16:04:11 +0800

    role update

--- auth/role.go
+++ auth/role.go
@@ -1,40 +1,77 @@
 package auth
 
 type role interface {
-	Create()
-	Delete()
-	Grant()
+	Create() error
+	Delete() error
+	Grant() error
 }
 
 const (
 	// 角色身份
-	ROLE_IDENTIDY_SUPER = 1 // 系统管理员
-	ROLE_IDENTIDY_ADMIN = 2 // 项目管理员
-	ROLE_IDENTIDY_OPS   = 3 // 运维工程师
-	ROLE_IDENTIDY_DEV   = 4 // 研发工程师
-	ROLE_IDENTIDY_TEST  = 5 // 测试工程师
+	ROLE_IDENTIDY_SUPER = iota + 1 // SRE - 系统管理员
+	ROLE_IDENTIDY_PRO   // SRE - 项目管理员
+	ROLE_IDENTIDY_OPS   // SRE - 运维工程师
+	ROLE_IDENTIDY_DEV   // 研发工程师
+	ROLE_IDENTIDY_TEST  // 测试工程师
 )
 
 type Identity int
 
 type Role struct {
 	Identity Identity // 身份
+	Name     string   // 名称
+	SREFlag  bool     // 是否SRE团队成员
 }
 
-func NewRole(identity Identity) *Role {
+func GetRoleName(id Identity) string {
+	roleNames := map[Identity]string{
+		ROLE_IDENTIDY_SUPER: "系统管理员",
+		ROLE_IDENTIDY_PRO: "项目管理员",
+		ROLE_IDENTIDY_OPS: "运维工程师",
+		ROLE_IDENTIDY_DEV: "研发工程师",
+		ROLE_IDENTIDY_TEST: "测试工程师",
+	}
+
+	if name, ok := roleNames[id]; ok {
+		return name
+	}
+
+	return "未授权人员"
+}
+
+// SRE 标志
+func GetSREFlag(id Identity) bool {
+	roleNames := map[Identity]bool{
+		ROLE_IDENTIDY_SUPER: true,
+		ROLE_IDENTIDY_PRO: true,
+		ROLE_IDENTIDY_OPS: true,
+		ROLE_IDENTIDY_DEV: false,
+		ROLE_IDENTIDY_TEST: false,
+	}
+
+	if flag, ok := roleNames[id]; ok {
+		return flag
+	}
+
+	return false
+}
+
+func NewRole(identity Identity) role {
 	return &Role{
 		Identity: identity,
+		Name: GetRoleName(identity),
+		SREFlag: GetSREFlag(identity),
 	}
 }
 
-func (r *Role) Create() {
-
+func (r *Role) Create() error {
+	return nil
 }
 
-func (r *Role) Delete() {
-
+func (r *Role) Delete() error {
+	return nil
 }
 
-func (r *Role) Grant() {
-
+func (r *Role) Grant() error {
+	return nil
 }
