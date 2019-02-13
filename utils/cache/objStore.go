26d79cbdfe5a1e18b392718ed179393eb55d68e7
chenjiefeng <chenjiefeng@4399inc.com>
2019-02-13 18:08:43 +0800

    update

--- utils/cache/objStore.go
+++ utils/cache/objStore.go
@@ -1,6 +1,10 @@
 package cache
 
-import "errors"
+import (
+	"errors"
+	"container/list"
+	"sync"
+)
 
 const (
 	OBJECT_STORE_MAX_CAP = 1000
@@ -23,6 +27,10 @@ type ObjStore struct {
 	cache map[interface{}]IThreadSafeMap
 	cap int
 	len int
+
+	lock sync.RWMutex
+
+	keys *list.List
 }
 
 func NewObjStore(cap int) IObjStore {
@@ -33,6 +41,7 @@ func NewObjStore(cap int) IObjStore {
 		cache: make(map[interface{}]IThreadSafeMap, cap),
 		cap: cap,
 		len: 0,
+		lock: sync.RWMutex{},
 	}
 }
 
@@ -45,6 +54,12 @@ func (obj *ObjStore) Add(section interface{}, key string, value interface{}) err
 	}
 	if value == nil {
 		return errors.New("value is nil")
+	}
+
+	// 容量判断
+	if obj.Len() >= OBJECT_STORE_MAX_CAP {
+		// 删除最近最少访问
+
 	}
 
 	if sectionMap, ok := obj.cache[section]; ok {
@@ -70,6 +85,10 @@ func (obj *ObjStore) Get(section interface{}, key string) (interface{}, error) {
 	}
 
 	if data, ok := obj.cache[section]; ok {
+		if ok, item := obj.exist(key); ok {
+			// 数据访问则把数据移动到头部
+			obj.keys.MoveToFront(item)
+		}
 		return data, nil
 	}
 	return nil, errors.New(key + " not found")
@@ -87,7 +106,20 @@ func (obj *ObjStore) Exist(section interface{}, key string) bool {
 	return ok
 }
 
+func (obj *ObjStore) exist(section string) (bool, *list.Element) {
+	for v := obj.keys.Front(); v != nil; v = v.Next() {
+		if section == v.Value.(string) {
+			return true, v
+		}
+	}
+
+	return false, nil
+}
+
 func (obj *ObjStore) Len() int {
+	obj.lock.RLock()
+	defer obj.lock.RUnlock()
+
 	return obj.len
 }
 
